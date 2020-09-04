6d08ac4b2fb481ef2e59980be2a83d00d9c9982e
Sjon Kosse <sjon@x61.eu>
2020-09-04 18:18:57 +0200

    Added DAA testing.

--- Tests/Increments.cs
+++ Tests/Increments.cs
@@ -133,5 +133,98 @@ namespace Tests
                 Assert.IsTrue(dec.Registers.Get(Flag.NH));
             }
         }
+        [Test]
+        public void ADD_A_B()
+        {
+            var dec = new Decoder(() => 0);
+
+            {
+                dec.Registers.Set(Register.A, 0x73);
+                dec.Registers.Set(Register.B, 0x26);
+
+                dec.StdOps[Unprefixed.ADD_A_B]();
+
+                Assert.AreEqual(0x99, dec.Registers.Get(Register.A));
+            }
+            {
+                dec.Registers.Set(Register.A, 0xF0);
+                dec.Registers.Set(Register.B, 0x0F);
+
+                dec.StdOps[Unprefixed.ADD_A_B]();
+
+                Assert.AreEqual(0xFF, dec.Registers.Get(Register.A));
+                Assert.IsTrue(dec.Registers.Get(Flag.NC));
+            }
+            {
+                dec.Registers.Set(Register.A, 0xF0);
+                dec.Registers.Set(Register.B, 0x10);
+
+                dec.StdOps[Unprefixed.ADD_A_B]();
+
+                Assert.AreEqual(0, dec.Registers.Get(Register.A));
+                Assert.IsTrue(dec.Registers.Get(Flag.C));
+            }
+        }
+        [Test]
+        public void DAA_wrap_around()
+        {
+            var dec = new Decoder(() => 0);
+
+            {
+                dec.Registers.Set(Register.A, 0x73);
+                dec.Registers.Set(Register.B, 0x27);
+
+                dec.StdOps[Unprefixed.ADD_A_B]();
+
+                Assert.AreEqual(0x9a, dec.Registers.Get(Register.A));
+
+                dec.StdOps[Unprefixed.DAA]();
+
+                Assert.AreEqual(0x00, dec.Registers.Get(Register.A));
+                Assert.IsTrue(dec.Registers.Get(Flag.C));
+            }
+        }
+        [Test]
+        public void DAA_99()
+        {
+            var dec = new Decoder(() => 0);
+
+
+            {
+                dec.Registers.Set(Register.A, 0x73);
+                dec.Registers.Set(Register.B, 0x26);
+
+                dec.StdOps[Unprefixed.ADD_A_B]();
+
+                Assert.AreEqual(0x99, dec.Registers.Get(Register.A));
+
+                dec.StdOps[Unprefixed.DAA]();
+
+                Assert.AreEqual(0x99, dec.Registers.Get(Register.A));
+                Assert.IsTrue(dec.Registers.Get(Flag.NC));
+            }
+
+        }
+        [Test]
+        public void DAA_83()
+        {
+            var dec = new Decoder(() => 0);
+
+
+            {
+                dec.Registers.Set(Register.A, 0x45);
+                dec.Registers.Set(Register.B, 0x38);
+
+                dec.StdOps[Unprefixed.ADD_A_B]();
+
+                Assert.AreEqual(0x7D, dec.Registers.Get(Register.A));
+
+                dec.StdOps[Unprefixed.DAA]();
+
+                Assert.AreEqual(0x83, dec.Registers.Get(Register.A));
+                Assert.IsTrue(dec.Registers.Get(Flag.NC));
+            }
+
+        }
     }
 }
