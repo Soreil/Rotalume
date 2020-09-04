8c43a8c29d67db8325147f99a1032a986fc705e2
Sjon Kosse <sjon@x61.eu>
2020-09-04 18:19:11 +0200

    Rotate tests

--- Tests/Shifts.cs
+++ Tests/Shifts.cs
@@ -40,5 +40,68 @@ namespace Tests
                 Assert.IsTrue(dec.Registers.Get(Flag.C));
             }
         }
+        [Test]
+        public void RRCA()
+        {
+            var dec = new Decoder(() => 0);
+
+            {
+                dec.Registers.Set(Register.A, 0x01);
+
+                dec.StdOps[Unprefixed.RRCA]();
+
+                Assert.AreEqual(0x80, dec.Registers.Get(Register.A));
+
+                Assert.IsTrue(dec.Registers.Get(Flag.C));
+            }
+            {
+                dec.Registers.Set(Register.A, 0xFF);
+
+                dec.StdOps[Unprefixed.RRCA]();
+
+                Assert.AreEqual(0xFF, dec.Registers.Get(Register.A));
+
+                Assert.IsTrue(dec.Registers.Get(Flag.C));
+            }
+            {
+                dec.Registers.Set(Register.A, 0);
+
+                dec.StdOps[Unprefixed.RRCA]();
+
+                Assert.AreEqual(0, dec.Registers.Get(Register.A));
+
+                Assert.IsTrue(dec.Registers.Get(Flag.NC));
+            }
+        }
+        [Test]
+        public void CPL()
+        {
+            var dec = new Decoder(() => 0);
+
+            dec.Registers.A.Write(0xff);
+            dec.StdOps[Unprefixed.CPL]();
+
+            Assert.AreEqual(0, dec.Registers.A.Read());
+        }
+        [Test]
+        public void SCF()
+        {
+            var dec = new Decoder(() => 0);
+
+            dec.StdOps[Unprefixed.SCF]();
+
+            Assert.IsTrue(dec.Registers.Get(Flag.C));
+        }
+        [Test]
+        public void CCF()
+        {
+            var dec = new Decoder(() => 0);
+
+            dec.StdOps[Unprefixed.CCF]();
+            Assert.IsTrue(dec.Registers.Get(Flag.C));
+
+            dec.StdOps[Unprefixed.CCF]();
+            Assert.IsTrue(dec.Registers.Get(Flag.NC));
+        }
     }
 }
