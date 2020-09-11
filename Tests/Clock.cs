48b8fef5fbd2c83fa092772350c1339e9ecad741
Sjon Kosse <sjon@x61.eu>
2020-09-11 22:27:35 +0200

    whitespace

--- Tests/Clock.cs
+++ Tests/Clock.cs
@@ -16,9 +16,7 @@ namespace Tests
         [Test]
         public void C2DoesNotThrow()
         {
-            var p = new BootBase(new List<byte>
-            { 0xc2, 0,0 }
-                    );
+            var p = new BootBase(new List<byte> { 0xc2, 0, 0 });
             p.DoNextOP();
         }
 
@@ -29,19 +27,14 @@ namespace Tests
             {
                 if (illegalOps.Contains((Unprefixed)i)) continue; //FIXME: when we implement halt and stop there should be some time taken here most likely.
 
-                var p = new BootBase(new List<byte>
-            { (byte)i, 0,0 }
-                );
+                var p = new BootBase(new List<byte> { (byte)i, 0, 0 });
 
                 p.DoNextOP();
-
                 Assert.AreNotEqual(0, p.Clock);
             }
             for (int i = 0; i < 0x100; i++)
             {
-                var p = new BootBase(new List<byte>
-            {0xCB, (byte)i, 0,0 }
-                );
+                var p = new BootBase(new List<byte> { 0xCB, (byte)i, 0, 0 });
 
                 p.DoNextOP();
                 Assert.AreNotEqual(0, p.Clock);
