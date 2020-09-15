2def3ea259abef09786953c916564211a4cf09f1
Sjon Kosse <sjon@x61.eu>
2020-09-15 12:51:19 +0200

    We are never going to  be setting most of these fields so there is no real reason to do full syntax getter setter properties.

--- Tests/PPU.cs
+++ Tests/PPU.cs
@@ -2,23 +2,23 @@
 
 namespace Tests
 {
-    public record PPU
+    public class PPU
     {
         //FF40 - FF4B, PPU control registers
-       public byte SCY; //FF42
-       public byte SCX; //FF43
-       public byte LY; //FF44
-       public byte LYC; //FF45
-       public byte WV; //FF4A
-       public byte WX; //FF4B
-        
-       public byte BGP; //FF47
-       public byte OBP0; //FF48
-       public byte OBP1; //FF49
-        
-       public byte DMA; //FF46
-        
-       public byte LCDC; //FF40
+        public byte SCY; //FF42
+        public byte SCX; //FF43
+        public byte LY; //FF44
+        public byte LYC; //FF45
+        public byte WV; //FF4A
+        public byte WX; //FF4B
+
+        public byte BGP; //FF47
+        public byte OBP0; //FF48
+        public byte OBP1; //FF49
+
+        public byte DMA; //FF46
+
+        public byte LCDC; //FF40
         ushort TileMapDisplaySelect
         {
             get
@@ -28,32 +28,9 @@ namespace Tests
             }
         }
 
-        ushort BGAndWindowTileDataSelect
-        {
-            get
-            {
-                if (LCDC.GetBit(4)) return 0x8000;
-                else return 0x8800;
-            }
-        }
-        ushort BGTileMapDisplaySelect
-        {
-            get
-            {
-                if (LCDC.GetBit(3)) return 0x9c00;
-                else return 0x9800;
-            }
-        }
-
-        int SpriteHeight
-        {
-            get
-            {
-                if (LCDC.GetBit(2)) return 16;
-                else return 8;
-            }
-        }
-
+        ushort BGAndWindowTileDataSelect => LCDC.GetBit(4) ? 0x8000 : 0x8800;
+        ushort BGTileMapDisplaySelect => LCDC.GetBit(3) ? 0x9c00 : 0x9800;
+        int SpriteHeight => LCDC.GetBit(2) ? 16 : 8;
         private bool LCDEnable() => LCDC.GetBit(7);
         private bool WindowDisplayEnable() => LCDC.GetBit(5);
         private bool OBJDisplayEnable() => LCDC.GetBit(1);
