f33442c007e6196005e78f9bd0e230d4e69337f6
Sjon Kosse <sjon@x61.eu>
2020-11-25 01:51:32 +0100

    This currently seems to be mostly correct but the tiles are corrupted. Scrolling seems to work.

--- generator/graphics/Renderer.cs
+++ generator/graphics/Renderer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+
 
 namespace generator
 {
@@ -8,6 +10,7 @@ namespace generator
         readonly PPU PPU;
         readonly Func<int> Clock;
         public int TimeUntilWhichToPause;
+        readonly StreamWriter fs;
 
         const int DrawlinesPerFrame = 144;
         const int ScanlinesPerFrame = DrawlinesPerFrame + 10;
@@ -28,6 +31,8 @@ namespace generator
             PPU = ppu;
             var startTime = PPU.Clock();
             Clock = () => ppu.Clock() - startTime;
+
+            fs = new(@"screens.txt");
         }
 
         public List<SpriteAttributes> SpriteAttributes = new();
@@ -36,51 +41,93 @@ namespace generator
         {
             var currentTime = Clock();
 
-            if (PPU.Mode == Mode.OAMSearch)
-                SpriteAttributes = PPU.OAM.SpritesOnLine(PPU.LY);
-            if (PPU.Mode == Mode.Transfer)
-                Draw();
-
-            if (currentTime > TimeUntilWhichToPause)
+            if (currentTime >= TimeUntilWhichToPause)
             {
+                if (PPU.Mode == Mode.OAMSearch)
+                    SpriteAttributes = PPU.OAM.SpritesOnLine(PPU.LY);
+                if (PPU.Mode == Mode.Transfer)
+                    Draw();
+
                 UpdateLineRegister();
                 IncrementMode();
                 SetNewClockTarget();
             }
         }
 
+
         private void Draw()
         {
-            int totalCycles = 0;
-
-            GetTile();
+            var line = GetLine();
+            fs.Write(line);
+            fs.Write(' ');
+            fs.Write(PPU.LY);
+            fs.Write(' ');
+            fs.Write(ClocksInFrame);
+            fs.WriteLine();
         }
-
-        private void GetTile()
+        private string GetLine()
         {
-            var yOffset = (PPU.LY + PPU.SCY) & 0xff;
+            var palette = GetPalette();
 
-            var tilemap = PPU.BGTileMapDisplaySelect; //This one has the background tiles, the other similar
+            var pixels = new Shade[160];
+
+            var yOffset = (PPU.LY + PPU.SCY) & 0xff;
+            var tilemap = PPU.BGTileMapDisplaySelect;
+
             for (int tileNumber = 0; tileNumber < 20; tileNumber++)
             {
-                var xOffset = ((PPU.SCX / 8) + tileNumber) & 0x1f;
-                //named field has the sprite tiles. We don't need sprites for the logo
-
-                var currentTileIndex = PPU.VRAM[tilemap + xOffset + (yOffset * 4)];
-
-                var tileData = PPU.BGAndWindowTileDataSelect;
-
-                var at = tileData + (currentTileIndex * 16) + (yOffset % 8 * 2);
-
-                var tileDataLow = PPU.VRAM[at];
-                var tileDataHigh = PPU.VRAM[at + 1]; //This location is incorrect
-
-                if (tileDataLow + tileDataHigh != 0)
-                {
-                    System.Diagnostics.Debugger.Break();
-                }
+                var curPix = TilePixelLine(palette, yOffset, tilemap, tileNumber);
+                for (int cur = 0; cur < curPix.Length; cur++)
+                    pixels[tileNumber * curPix.Length + cur] = curPix[cur];
             }
+
+            char[] ScreenLine = new char[160];
+            for (var p = 0; p < pixels.Length; p++)
+            {
+                if (pixels[p] == Shade.White) ScreenLine[p] = '.';
+                else if (pixels[p] == Shade.Black) ScreenLine[p] = '#';
+                else throw new ArgumentException("Expected a white or black pixel only in the boot up screen");
+            }
+
+            var s = new string(ScreenLine);
+            return s;
         }
+
+        private Shade[] TilePixelLine(Shade[] palette, int yOffset, ushort tilemap, int tileNumber)
+        {
+            var xOffset = ((PPU.SCX / 8) + tileNumber) & 0x1f;
+
+            var currentTileIndex = PPU.VRAM[tilemap + xOffset + (yOffset * 4)]; //Background ID map is laid out as 32x32 tiles of size 8x8
+
+            var tileData = PPU.BGAndWindowTileDataSelect;
+
+            var pixels = new Shade[8];
+
+            //16 bytes per tile so times 16 on the tileindex
+            //We need the line in the 8x8 tile so we take y mod 8 to get it
+            //Times 2 is needed because a tile has two bytes per line.
+            var at = tileData + (currentTileIndex * 16) + ((yOffset % 8) * 2);
+
+            var tileDataLow = PPU.VRAM[at]; //low byte of line
+            var tileDataHigh = PPU.VRAM[at + 1]; //high byte of line
+
+            for (int i = 7; i >= 0; i--) //tilesIDs are stored with the first pixel at MSB
+            {
+                var paletteIndex = tileDataLow.GetBit(i) ? 1 : 0;
+                paletteIndex += tileDataHigh.GetBit(i) ? 2 : 0;
+
+                pixels[7 - i] = palette[paletteIndex]; //We want the leftmost bit on the left
+            }
+
+            return pixels;
+        }
+
+        private Shade[] GetPalette() => new Shade[4] {
+                PPU.BackgroundColor(0),
+                PPU.BackgroundColor(1),
+                PPU.BackgroundColor(2),
+                PPU.BackgroundColor(3)
+            };
 
         private void IncrementMode()
         {
@@ -98,6 +145,7 @@ namespace generator
             else
             {
                 PPU.Mode = Mode.VBlank;
+                fs.WriteLine();
             }
         }
 
