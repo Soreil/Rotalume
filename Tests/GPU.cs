4689f59c396c907eaf53bcdb0d484b59d0ecd734
Sjon Kosse <sjon@x61.eu>
2020-12-31 00:01:01 +0100

    Working on a bug where our stack is corrupted because the stack pointer is in high memory.

--- Tests/GPU.cs
+++ Tests/GPU.cs
@@ -170,8 +170,6 @@ namespace Tests
         {
             Core Proc = new Core(Core.LoadBootROM(), LoadGameROM());
 
-            Proc.PPU.Writer = new FrameSink(x => _ = x);
-
             //LCD is off
             Assert.AreEqual(Proc.PPU.LCDC, 0);
             //No palette
@@ -200,7 +198,6 @@ namespace Tests
         {
             Core Proc = new Core(Core.LoadBootROM(), LoadGameROM());
 
-            Proc.PPU.Writer = new FrameSink(x => _ = x);
             while (Proc.PC != 0x100)
                 Proc.Step();
 
@@ -250,6 +247,92 @@ namespace Tests
 
             Assert.AreEqual(0x27a3, Proc.PC);
         }
+        [Test]
+        public void TetrisDMA()
+        {
+            Core Proc = new Core(Core.LoadBootROM(), LoadGameROM());
 
+            int dmaCount = 0;
+            var SPOnCall = 0;
+            var SPBefore = 0;
+            var SPAfter = 0;
+            while (dmaCount != 0x17a7)
+            {
+                //while (Proc.PC != 0x01d5) Proc.Step();
+
+                //while (Proc.PC != 0xffb6) Proc.Step();
+
+                ////We are now in DMA
+                //while (Proc.PC != 0xffbf) Proc.Step();
+                ////Return time
+                //System.Console.WriteLine("From:{0:X}", Proc.PC);
+                //Proc.Step();
+                //System.Console.WriteLine("To:{0:X}", Proc.PC);
+                ////return addr loaded by ret
+                //System.Console.WriteLine();
+
+                Proc.Step();
+                if (Proc.PC == 0x01d5)
+                {
+                    SPOnCall = Proc.CPU.Registers.SP;
+                }
+                if (Proc.PC == 0xffbf)
+                {
+                    dmaCount++;
+                }
+            }
+
+
+            while (Proc.PC != 0x01d5) Proc.Step();
+
+            while (Proc.PC != 0xffb6)
+            {
+                System.Console.WriteLine("Stack value going to ffb6:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+                System.Console.WriteLine("PC going to ffb6:{0:X}", Proc.PC);
+                Proc.Step();
+            }
+            System.Console.WriteLine("Stack value at ffb6:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+            System.Console.WriteLine("PC at ffb6:{0:X}", Proc.PC);
+
+
+            Proc.Step();
+            System.Console.WriteLine("Stack value going to ffb8:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+            System.Console.WriteLine("PC going to ffb8:{0:X}", Proc.PC);
+
+
+            //Our stack pointer it set to a position in FFxx where LDH is going to write to execute
+            //The DMA transfer. This happens to overwrite our stack. BGB has a stackpointer in WRAM
+            //At CFF3. Our stack pointer should be somewhere there as well.
+            Proc.Step();
+            System.Console.WriteLine("Stack value at ffb8:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+            System.Console.WriteLine("PC at ffb8:{0:X}", Proc.PC);
+
+            //Pausing for DMA duration
+            while (Proc.PC != 0xffbf)
+            {
+                Proc.Step();
+                System.Console.WriteLine("Stack value going to ffbf:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+                System.Console.WriteLine("PC going to ffbf:{0:X}", Proc.PC);
+            }
+
+            //Return time
+            System.Console.WriteLine("From:{0:X} PC at ffbf", Proc.PC);
+            System.Console.WriteLine("Stack value at ffbf:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+            Proc.Step();
+            System.Console.WriteLine("To:{0:X} PC after ffbf", Proc.PC);
+            System.Console.WriteLine("Stack value after ffbf:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+            //return addr loaded by ret
+            System.Console.WriteLine();
+
+
+            SPBefore = Proc.CPU.Registers.SP;
+            Proc.Step();
+            System.Console.WriteLine("Stack value after returning:{0:X}", Proc.CPU.Memory.ReadWide(Proc.CPU.Registers.SP));
+            SPAfter = Proc.CPU.Registers.SP;
+            //0x17a8 here for some reason. If SP was 1 different we would have gotten expected 01d8
+            //Something is changing the stackpointer in between the call to DMA function and return?
+            Assert.AreEqual(0x01d8, Proc.PC);
+            Assert.AreEqual(SPOnCall, SPBefore);
+        }
     }
 }
