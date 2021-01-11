9efd0955a0de3ae5e290f027d5ecf2f57f4b93ab
Sjon Kosse <sjon@x61.eu>
2021-01-12 00:07:52 +0100

    Was checking to see if keyboard interrupts was enabled before setting the fired bit instead of just setting it either way.

--- generator/Core.cs
+++ generator/Core.cs
@@ -246,7 +246,7 @@ namespace emulator
             if (op != 0xcb)
             {
                 //if ((Unprefixed)op != Unprefixed.CPL && (Unprefixed)op != Unprefixed.NOP && (Unprefixed)op != Unprefixed.RST_38H)
-                Unprefixeds.Push((PC - 1, (Unprefixed)op));
+                //Unprefixeds.Push((PC - 1, (Unprefixed)op));
                 CPU.Op((Unprefixed)op)();
             }
             else
@@ -288,7 +288,7 @@ namespace emulator
         {
             DoNextOP();
             //We really should have the GUI thread somehow do this logic but polling like this should work
-            if (!InterruptControlRegister.GetBit(4) && GetKeyboardInterrupt())
+            if (!InterruptFireRegister.GetBit(4) && GetKeyboardInterrupt())
                 InterruptFireRegister = InterruptFireRegister.SetBit(4);
 
             DoInterrupt();
