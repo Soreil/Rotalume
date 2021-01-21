d049b25271437142fed01416a314a895d00c9023
Sjon Kosse <sjon@x61.eu>
2021-01-21 12:16:50 +0100

    Add notes for when I get around to adding multicart handling

--- emulator/MBC1.cs
+++ emulator/MBC1.cs
@@ -3,7 +3,23 @@ using System;
 
 namespace emulator
 {
-    //MBC1 does not currently do multicart detection and as such won't work correctly since multicarts have different wiring
+    //TODO: Add heuristic detection of multicart ROMs.
+
+    /*From Gameboy Complete Technical Reference by Gekkio:
+
+    Detecting multicarts
+    MBC1 multicarts are not detectable by simply looking at the ROM header, because the ROM type value is just
+    one of the normal MBC1 values. However, detection is possible by going through BANK2 values and looking
+    at "bank 0" of each multicart game and doing some heuristics based on the header data. All the included
+    games, including the game selection menu, have proper header data. One example of a good heuristic is logo
+    data verification.
+    So, if you have a 8 Mbit cart with MBC1, first assume that it’s a multicart and bank numbers are 6-bit
+    values. Set BANK1 to zero and loop through the four possible BANK2 values while checking the data at
+    0x0104-0x0133. In other words, check logo data starting from physical ROM locations 0x00104, 0x40104,
+    0x80104, and 0xC0104. If proper logo data exists with most of the BANK2 values, the cart is most likely a
+    multicart. Note that multicarts can just have two actual games, so one of the locations might not have the
+    header data in place.
+    */
     internal class MBC1 : MBC
     {
         private readonly byte[] gameROM;
