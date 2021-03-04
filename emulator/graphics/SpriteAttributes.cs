c39992461a484bbff68e868d36196fd9554c47eb
Sjon Kosse <sjon@x61.eu>
2021-03-04 14:26:13 +0100

    Replace spriteattributes with a struct to avoid allocations

--- emulator/graphics/SpriteAttributes.cs
+++ emulator/graphics/SpriteAttributes.cs
@@ -2,11 +2,23 @@
 
 namespace emulator
 {
-    public record SpriteAttributes(byte Y, byte X, byte ID, byte Flags)
+    public struct SpriteAttributes
     {
         public bool SpriteToBackgroundPriority => Flags.GetBit(7);
         public bool YFlipped => Flags.GetBit(6);
         public bool XFlipped => Flags.GetBit(5);
         public int Palette => Convert.ToInt32(Flags.GetBit(4));
+
+        public byte Y;
+        public byte X;
+        public byte ID;
+        public byte Flags;
+        public SpriteAttributes(byte Y, byte X, byte ID, byte Flags)
+        {
+            this.Y = Y;
+            this.X = X;
+            this.ID = ID;
+            this.Flags = Flags;
+        }
     }
 }
