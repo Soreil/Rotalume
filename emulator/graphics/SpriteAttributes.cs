41a072027cb4f5ad5cffdd8fb237fa0c19baa79c
Sjon Kosse <sjon@x61.eu>
2021-03-12 20:09:43 +0100

    Simplify SpriteAttributes struct

--- emulator/graphics/SpriteAttributes.cs
+++ emulator/graphics/SpriteAttributes.cs
@@ -1,24 +1,32 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace emulator
 {
+    [StructLayout(LayoutKind.Sequential)]
     public readonly struct SpriteAttributes : IComparable<SpriteAttributes>
     {
-        public bool SpriteToBackgroundPriority => Flags.GetBit(7);
-        public bool YFlipped => Flags.GetBit(6);
-        public bool XFlipped => Flags.GetBit(5);
-        public int Palette => Convert.ToInt32(Flags.GetBit(4));
-
         public readonly byte Y;
         public readonly byte X;
         public readonly byte ID;
         public readonly byte Flags;
+
+        public readonly byte Palette;
+        public readonly bool SpriteToBackgroundPriority;
+        public readonly bool YFlipped;
+        public readonly bool XFlipped;
         public SpriteAttributes(byte Y, byte X, byte ID, byte Flags)
         {
             this.Y = Y;
             this.X = X;
             this.ID = ID;
             this.Flags = Flags;
+
+            SpriteToBackgroundPriority = Flags.GetBit(7);
+            YFlipped = Flags.GetBit(6);
+            XFlipped = Flags.GetBit(5);
+            Palette = Convert.ToByte(Flags.GetBit(4));
+
         }
 
         int IComparable<SpriteAttributes>.CompareTo(SpriteAttributes other) => X.CompareTo(other.X);
