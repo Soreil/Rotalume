fe3e0df412014c83139986780c06578075c23142
Sjon Kosse <sjon@x61.eu>
2021-03-23 13:57:02 +0100

    Simplified falling edge detection for TIMA increment

--- emulator/memory/Timers.cs
+++ emulator/memory/Timers.cs
@@ -33,12 +33,12 @@ namespace emulator
                 }
             }
 
-            if (TimerEnabled)
+            if (TACEnable)
             {
 
                 //Overflowing internalcounter shouldn't be an issue here.
-                var overflow = ((InternalCounter + 1) & (1 << TACBitSelected)) == 0;
-                if (overflow && (InternalCounter & (1 << TACBitSelected)) == 0 == false)
+                var overflow = ((InternalCounter + 1) & TACFrequency) == 0;
+                if (overflow && (InternalCounter & TACFrequency) != 0)
                 {
                     IncrementTIMA();
                 }
@@ -51,7 +51,7 @@ namespace emulator
             get => (byte)(InternalCounter >> 8);
             set
             {
-                var overflow = (InternalCounter & (1 << TACBitSelected)) != 0;
+                var overflow = (InternalCounter & TACFrequency) != 0;
                 if (overflow)
                 {
                     IncrementTIMA();
@@ -63,48 +63,48 @@ namespace emulator
 
         public byte TAC
         {
-            get => (byte)(0xf8 | ((TimerEnabled ? 1 : 0) << 2) | PositionToBits(TACBitSelected));
+            get => (byte)(0xf8 | ((TACEnable ? 1 : 0) << 2) | PositionToBits(TACFrequency));
             set
             {
                 bool glitch;
-                if (!TimerEnabled)
+                if (!TACEnable)
                 {
                     glitch = false;
                 }
                 else
                 {
                     glitch = !value.GetBit(2)
-                        ? (InternalCounter & (1 << (TACBitSelected))) != 0
-                        : ((InternalCounter & (1 << (TACBitSelected))) != 0) &&
-                                 ((InternalCounter & (1 << (BitPosition(value)))) == 0);
+                        ? (InternalCounter & TACFrequency) != 0
+                        : ((InternalCounter & TACFrequency) != 0) &&
+                                 ((InternalCounter & (BitPosition(value))) == 0);
                 }
                 if (glitch)
                 {
                     IncrementTIMA();
                 }
 
-                TimerEnabled = value.GetBit(2);
-                TACBitSelected = BitPosition(value);
+                TACEnable = value.GetBit(2);
+                TACFrequency = BitPosition(value);
             }
         }
 
-        private bool TimerEnabled;
-        private int TACBitSelected;
+        private bool TACEnable;
+        private int TACFrequency;
 
-        private static byte BitPosition(byte b) => (b & 0x03) switch
+        private static int BitPosition(byte b) => (b & 0x03) switch
         {
-            0 => 9,
-            1 => 3,
-            2 => 5,
-            3 => 7,
+            0 => 1 << 9,
+            1 => 1 << 3,
+            2 => 1 << 5,
+            3 => 1 << 7,
             _ => throw new NotImplementedException(),
         };
         private static byte PositionToBits(int p) => p switch
         {
-            9 => 0,
-            3 => 1,
-            5 => 2,
-            7 => 3,
+            1 << 9 => 0,
+            1 << 3 => 1,
+            1 << 5 => 2,
+            1 << 7 => 3,
             _ => throw new NotImplementedException(),
         };
 
